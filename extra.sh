f9d80e75985bf9d5948c38810494c06c2050b5a9
Sebastien Valette <sebastien.valette@creatis.insa-lyon.fr>
2020-10-06 14:03:30 +0200

    rework building

--- extra.sh
+++ extra.sh
@@ -1,8 +1,8 @@
-for outpuPath in compiled/build compiled/source
+#!/bin/bash
+time=$(date +%s)
+for outpuPath in compiled/source compiled/build dist
 do
-cp icon.* $outpuPath
-cp splash.* $outpuPath
 cp script.js $outpuPath
 cp package.json $outpuPath
-cp buildDate.txt $outpuPath
+sed -i -- 's/${compileTime}/'$time'/g' $outpuPath/index.html
 done
